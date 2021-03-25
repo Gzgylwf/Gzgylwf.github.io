8625e7699785797dbc8ced618f0e60b85d0b70f4
Li Wenfeng <gemini_china@hotmail.com>
2021-03-25 17:52:47 +0800

    About & Experience template finished.

--- update_site.py
+++ update_site.py
@@ -2,17 +2,32 @@
 from brokers import *
 from jinja2 import Environment, FileSystemLoader
 import io
+import pandas as pd
+import numpy as np
+import yaml
+
+
+def readResume(fn):
+    ret = yaml.load(open(fn, 'r'))
+    return ret
 
 
 if __name__ == "__main__":
+    resume = readResume('data/resume.yaml')
+    
     env = Environment(loader=FileSystemLoader("templates"))
     template = env.get_template('index.html')
     output = template.render(
-        title = "Personal Page - LI Wenfeng"
+        title = "Personal Page - LI Wenfeng",
+        sessAbout = resume['about'],
+        sessExp = resume['experience']
     )
 
     # Save to file
-    with io.open('index.html', "w") as f:
+    with io.open('index_test.html', "w") as f:
         f.write(output)
         f.close()
     print("Update finish!")
+    
+    print(resume['experience'])
+    
