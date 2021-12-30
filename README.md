4ec399702cf7a2818afa45f8113c178083c80716
Gzgylwf <gemini_china@hotmail.com>
2021-12-30 11:33:47 +0800

    Version 1.0: Complete the design and code.

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
-# Personal Page
+# Personal Page & Resume Editor
 
-For update the pages, run below command:
+The repo is used for update the personal page and resume.
+
+## 1. __Write the YAML__
+
+For the personal page, it will show all fields except `summary` session.
+
+For the resume page, it will only show the those field w/o `hidden` attributes.
+
+## 2. __Run the code__
 
 ```shell
 source env/bin/activate
 python update_site.py
 ```
+
+## 3. __Open and print the resume__
+
+Open the resume.html page and print out to replace the .pdf resume in the root directory. Then the personal page can download normally.
--- update_site.py
+++ update_site.py
@@ -2,38 +2,75 @@
 from brokers import *
 from jinja2 import Environment, FileSystemLoader
 import io
-import pandas as pd
-import numpy as np
+import os
 import yaml
 import pdfkit
 
 
 def readResume(fn):
-    ret = yaml.load(open(fn, 'r'))
+    ret = yaml.safe_load(open(fn, 'r'))
     return ret
 
+def saveResume(resume, fn):
+    yaml.safe_dump(resume, open(fn, 'w'))
 
-if __name__ == "__main__":
-    resume = readResume('data/resume.yaml')
-    
-    env = Environment(loader=FileSystemLoader("templates"))
-    template = env.get_template('index.html')
+def render(object, template, outputFn):
+    template = env.get_template(template)
     output = template.render(
-        title = "Personal Page - LI Wenfeng",
-        sessAbout = resume['about'],
-        sessExp = resume['experience'],
-        sessEdu = resume['education'],
-        sessSkill = resume['skills'],
-        sessAwards = resume['awards']
+        title = "Personal Page - {} {}".format(object['about']['lastName'], object['about']['firstName']),
+        sessAbout = object['about'],
+        sessExp = object['experience'],
+        sessEdu = object['education'],
+        sessSkill = object['skills'],
+        sessAwards = object['awards']
     )
-
     # Save to file
-    targetFn = 'index_test.html'
-    with io.open(targetFn, "w") as f:
+    with io.open(outputFn, "w") as f:
         f.write(output)
         f.close()
 
+if __name__ == "__main__":
+    resume = readResume('data/resume.yaml')
+    resumeCN = None
+    if os.path.exists('data/resume_cn.yaml'): resumeCN = readResume('data/resume_cn.yaml')
+    
+    env = Environment(loader=FileSystemLoader("templates"))
+    templatesFiles = ['index', 'resume']
+
+    for templateFile in templatesFiles:
+        render(resume, "{}.html".format(templateFile), "{}.html".format(templateFile))
+        if resumeCN is not None:
+            render(resumeCN, "{}.html".format(templateFile), "{}_cn.html".format(templateFile))
+        '''
+        template = env.get_template(templateFile)
+        output = template.render(
+            title = "Personal Page - LI Wenfeng",
+            sessAbout = resume['about'],
+            sessExp = resume['experience'],
+            sessEdu = resume['education'],
+            sessSkill = resume['skills'],
+            sessAwards = resume['awards']
+        )
+
+        # Save to file
+        with io.open(templateFile, "w") as f:
+            f.write(output)
+            f.close()
+        '''
+
     # Save pdf
-    #pdfkit.from_string(output, output_path='{}{}-resume.pdf'.format(resume['about']['lastName'], resume['about']['firstName']))
+    '''
+    options = {
+        'page-size': 'A4',
+        'margin-top': '0.0in',
+        'margin-right': '0.0in',
+        'margin-bottom': '0.0in',
+        'margin-left': '0.0in',
+        'encoding': "UTF-8",
+        'no-outline': None,
+        'enable-local-file-access': ""
+    }
+    pdfkit.from_file('resume.html', output_path='{}{}-resume.pdf'.format(resume['about']['lastName'], resume['about']['firstName']), options=options)
+    '''
     print("Update finish!")
     
